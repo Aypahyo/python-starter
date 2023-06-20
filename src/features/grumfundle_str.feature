619fc530a9134f077b9de635265fc99ffc075d20
Aypahyo <Johannes.Hirschmugl@gmx.net>
2023-06-20 11:49:43 +0200

    prettify behave tests

--- src/features/grumfundle_str.feature
+++ src/features/grumfundle_str.feature
@@ -3,5 +3,5 @@ Feature: showing off behave
   Scenario: run a simple test
      Given we have grumfundle 'foo'
       When we created a string 'foo-str' made from grumfundle 'foo' 
-      Then that string should contain 'foo'
-      And that string should contain 'Grumfundle'
+      Then 'foo-str' string should contain 'foo'
+      And 'foo-str' string should contain 'Grumfundle'
--- src/features/steps/grumfundle_str.py
+++ src/features/steps/grumfundle_str.py
@@ -1,14 +1,20 @@
 from behave import given, when, then # type: ignore
 from starter_core import grumfundler
 
-@given(u'we have grumfundle \'foo\'')
-def we_have_grumfundle(context):
-    context.grumfundle = grumfundler.Grumfundler('foo')
+def ensure_attribute(context, attribute_string):
+    if not hasattr(context, attribute_string):
+        setattr(context, attribute_string, {})
 
-@when(u'we created a string \'foo-str\' made from grumfundle \'foo\'')
-def we_created_string(context):
-    context.foo_str = str(context.grumfundle)
+@given(u'we have grumfundle \'{gf_key}\'')
+def we_have_grumfundle(context, gf_key):
+    ensure_attribute(context, 'grumfundles')
+    context.grumfundles[gf_key] = grumfundler.Grumfundler(gf_key)
 
-@then(u'that string should contain \'{string}\'')
-def string_should_contain(context, string):
-    assert string in context.foo_str
+@when(u'we created a string \'{str_key}\' made from grumfundle \'{gf_key}\'')
+def we_created_string(context, str_key, gf_key):
+    ensure_attribute(context, 'strings')
+    context.strings[str_key] = str(context.grumfundles[gf_key])
+
+@then(u'\'{str_key}\' string should contain \'{str_value}\'')
+def string_should_contain(context, str_key, str_value):
+    assert str_value in context.strings[str_key]
